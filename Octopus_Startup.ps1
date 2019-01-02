f82a3b61ec15f8c5310dec76206a4e305fb886c6
akozlov-firedev <46216318+akozlov-firedev@users.noreply.github.com>
2019-01-02 13:57:02 +0300

    Update Octopus_Startup.ps1

--- Octopus_Startup.ps1
+++ Octopus_Startup.ps1
@@ -1,4 +1,19 @@
-. docker-compose stop
+if ($dropscanContainerId = docker ps -q -f name=sd.dropscan.api.container-*) { 
+  docker rm -f $dropscanContainerId 
+}
+
+if ($simpledmsApiContainerId = docker ps -q -f name=sd.simpledms.api.container-*) { 
+  docker rm -f $simpledmsApiContainerId 
+}
+
+if ($simpledmsUiContainerId = docker ps -q -f name=sd.simpledms.ui.container-*) { 
+  docker rm -f $simpledmsUiContainerId 
+}
+
+if ($simpledmsDbContainerId = docker ps -q -f name=simple.dms.db.container-*) { 
+  docker rm -f $simpledmsDbContainerId 
+}
+
 . docker-compose down --rmi all -v
 . docker-compose up -d
 EXIT 0
