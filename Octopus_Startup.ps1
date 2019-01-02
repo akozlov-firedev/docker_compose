854b389cfdab80f9305ed097bc6afc6f3914f1f9
akozlov-firedev <46216318+akozlov-firedev@users.noreply.github.com>
2019-01-02 15:38:07 +0300

    Update Octopus_Startup.ps1

--- Octopus_Startup.ps1
+++ Octopus_Startup.ps1
@@ -1,16 +1,16 @@
-if ($dropscanContainerId = docker ps -q -f name=sd.dropscan.api.container-*) { 
+if ($dropscanContainerId = docker ps -a -q -f name=sd.dropscan.api.container-*) { 
   docker rm -f $dropscanContainerId 
 }
 
-if ($simpledmsApiContainerId = docker ps -q -f name=sd.simpledms.api.container-*) { 
+if ($simpledmsApiContainerId = docker ps -a -q -f name=sd.simpledms.api.container-*) { 
   docker rm -f $simpledmsApiContainerId 
 }
 
-if ($simpledmsUiContainerId = docker ps -q -f name=sd.simpledms.ui.container-*) { 
+if ($simpledmsUiContainerId = docker ps -a -q -f name=sd.simpledms.ui.container-*) { 
   docker rm -f $simpledmsUiContainerId 
 }
 
-if ($simpledmsDbContainerId = docker ps -q -f name=simple.dms.db.container-*) { 
+if ($simpledmsDbContainerId = docker ps -a -q -f name=simple.dms.db.container-*) { 
   docker rm -f $simpledmsDbContainerId 
 }
 
